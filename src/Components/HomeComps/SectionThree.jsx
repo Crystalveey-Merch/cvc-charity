7b11209e5e255bd06cb26c2efe1575d0e0a9cbf0
muzardemoses <muzardemoses@gmail.com>
2023-11-03 08:25:38 +0100

    ....

--- src/Components/HomeComps/SectionThree.jsx
+++ src/Components/HomeComps/SectionThree.jsx
@@ -36,7 +36,7 @@ export const SectionThree = () => {
                                 <p className="text-white">
                                     Read their stories
                                 </p>
-                                <p className="text-orange-700">
+                                <p className="text-white">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                                     </svg>
